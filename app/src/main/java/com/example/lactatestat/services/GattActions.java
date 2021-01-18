b1ae1946732c64a72157c9c82afa0a42e86cd988
Emil Ekelund <emil.ekelund@outlook.com>
2021-01-18 16:14:02 +0100

    Add GattActions class

---
+++ app/src/main/java/com/example/lactatestat/services/GattActions.java
@@ -0,0 +1,46 @@
+package com.example.lactatestat.services;
+
+public class GattActions {
+    /**
+     * The action corresponding to LactateStat events from BleService.
+     * Intended for IntentFilters for a BroadcastReceiver.
+     */
+    public final static String ACTION_GATT_LACTATESTAT_EVENT =
+            "com.example.lactatestat.services.ACTION_GATT_LACTATESTAT_EVENT";
+
+    /**
+     * A flag for event info in intents (via intent.putExtra)
+     */
+    public final static String EVENT =
+            "com.example.lactatestat.services.EVENT";
+
+    /**
+     * A flag for LactateStat data in intent (via intent.putExtra)
+     */
+    public final static String LACTATESTAT_DATA =
+            "com.example.lactatestat.services.LACTATESTAT_DATA";
+
+
+    /**
+     * Events corresponding to Gatt status/events
+     */
+    public enum Event {
+        GATT_CONNECTED("Connected"),
+        GATT_DISCONNECTED("Disconnected"),
+        GATT_SERVICES_DISCOVERED("Services discovered"),
+        LACTATESTAT_SERVICE_DISCOVERED("LactateStat Service"),
+        LACTATESTAT_SERVICE_NOT_AVAILABLE("LactateStat service unavailable"),
+        DATA_AVAILABLE("Data available");
+
+        @Override
+        public String toString() {
+            return text;
+        }
+
+        private final String text;
+
+        Event(String text) {
+            this.text = text;
+        }
+    }
+}
