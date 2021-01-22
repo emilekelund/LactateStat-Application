709c5b185ba8201f8aabea29f628429a7cab24f3
Emil Ekelund <emil.ekelund@outlook.com>
2021-01-22 10:52:50 +0100

    Change Launcher Icon

--- app/src/main/java/com/example/lactatestat/activities/SessionActivity.java
+++ app/src/main/java/com/example/lactatestat/activities/SessionActivity.java
@@ -1,10 +1,15 @@
 package com.example.lactatestat.activities;
 
 import android.bluetooth.BluetoothDevice;
+import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 public class SessionActivity extends AppCompatActivity {
     private static final String TAG = SessionActivity.class.getSimpleName();
@@ -20,5 +25,13 @@ public class SessionActivity extends AppCompatActivity {
     private TextView mBottomAxisLabel;
     private TextView mLeftAxisLabel;
 
+    private ILineDataSet set = null;
+    private LineChart mChart;
+    private Thread mThread;
+    private static boolean plotData = true;
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
