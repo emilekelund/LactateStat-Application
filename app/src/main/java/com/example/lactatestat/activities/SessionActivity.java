b32830888bf8d08a1ad7197b75da4030fa4aa010
Emil Ekelund <emil.ekelund@outlook.com>
2021-01-22 10:35:31 +0100

    Gradle update

--- app/src/main/java/com/example/lactatestat/activities/SessionActivity.java
+++ app/src/main/java/com/example/lactatestat/activities/SessionActivity.java
@@ -1,4 +1,24 @@
 package com.example.lactatestat.activities;
 
-public class SessionActivity {
+import android.bluetooth.BluetoothDevice;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SessionActivity extends AppCompatActivity {
+    private static final String TAG = SessionActivity.class.getSimpleName();
+    private static final String SELECTED_DEVICE = "selectedDevice";
+
+    private BluetoothDevice mSelectedDevice = null;
+    private String mDeviceAddress;
+
+    private TextView mCurrentView;
+    private TextView mLactateView;
+    private TextView mConnectionStatusView;
+    private ImageView mStatusIcon;
+    private TextView mBottomAxisLabel;
+    private TextView mLeftAxisLabel;
+
+
 }
