64d17f8a85a1b466ee59518f5245e4a420f5f915
Emil Ekelund <emil.ekelund@outlook.com>
2021-02-04 16:53:05 +0100

    Properly close gatt connection.

--- app/src/main/java/com/example/lactatestat/services/BleService.java
+++ app/src/main/java/com/example/lactatestat/services/BleService.java
@@ -265,7 +265,7 @@ public class BleService extends Service {
         // After using a given device, you should make sure that BluetoothGatt.close()
         // is called such that resources are cleaned up properly.  In this particular
         // example, close() is invoked when the UI is disconnected from the Service.
-        // close();
+        close();
         return super.onUnbind(intent);
     }
 
