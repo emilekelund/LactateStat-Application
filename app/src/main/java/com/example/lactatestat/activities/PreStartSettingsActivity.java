dc870da73bb08254c21a0f61332c11ca68b109fb
Emil Ekelund <emil.ekelund@outlook.com>
2021-06-09 10:59:41 +0200

    Implement logic for new calibration steps

--- app/src/main/java/com/example/lactatestat/activities/PreStartSettingsActivity.java
+++ app/src/main/java/com/example/lactatestat/activities/PreStartSettingsActivity.java
@@ -2,23 +2,31 @@ package com.example.lactatestat.activities;
 
 // Copyright (c) 2021 Emil Ekelund
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.lactatestat.R;
 
-import java.nio.charset.CharsetDecoder;
-import java.util.BitSet;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Objects;
 
 import static com.example.lactatestat.utilities.MessageUtils.createDialog;
@@ -31,6 +39,10 @@ public class PreStartSettingsActivity extends AppCompatActivity implements Adapt
     private static final String TIA_GAIN_INDEX = "tiaGainIndex";
     private static final String LOAD_RESISTOR_INDEX = "loadResistorIndex";
     private static final String INTERNAL_ZERO_INDEX = "internalZeroIndex";
+    private static final String SLOPE = "slope";
+    private static final String INTERCEPT = "intercept";
+
+    private TextView mCalibrationInfo;
 
     private BluetoothDevice mSelectedDevice = null;
     private Spinner mBiasVoltageSpinner;
@@ -43,6 +55,13 @@ public class PreStartSettingsActivity extends AppCompatActivity implements Adapt
     private int mTiaGainIndex = 7;
     private int mLoadResistorIndex = 3;
     private int mInternalZeroIndex = 0;
+
+    private boolean calibrationDone = false;
+
+    private float slope = 0;
+    private float intercept = 0;
+
+    private static final int CALIBRATION_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -64,6 +83,8 @@ public class PreStartSettingsActivity extends AppCompatActivity implements Adapt
                 onBackPressed();
             }
         });
+
+        mCalibrationInfo = findViewById(R.id.info_text);
 
         mBiasVoltageSpinner = findViewById(R.id.bias_voltage_spinner);
         mBiasVoltageSpinner.setOnItemSelectedListener(this);
@@ -139,6 +160,49 @@ public class PreStartSettingsActivity extends AppCompatActivity implements Adapt
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
         // Another interface callback
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CALIBRATION_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                slope = data.getFloatExtra(SLOPE, 0);
+                intercept = data.getFloatExtra(INTERCEPT, 0);
+                mCalibrationInfo.setText(String.format("New calibration done: f(x) = %.1e + %.1f\nReady to start session.", slope, intercept));
+                calibrationDone = true;
+            }
+
+        }
+
+        if (requestCode == 2
+                && resultCode == Activity.RESULT_OK) {
+            // The result data contains a URI for the document or directory that the user selected
+            Uri uri = null;
+            if (data != null) {
+                uri = data.getData();
+                try {
+                    InputStream inputStream = getContentResolver().openInputStream(uri);
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                    // Read the opened values from the inputStream
+
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        String[] rowData = line.split(",");
+                        slope = Float.parseFloat(rowData[0]);
+                        intercept = Float.parseFloat(rowData[1]);
+                    }
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                mCalibrationInfo.setText(String.format("Calibration loaded: f(x) = %.1e + %.1f\nReady to start session.", slope, intercept));
+                calibrationDone = true;
+
+            }
+        }
 
     }
 
@@ -150,6 +214,40 @@ public class PreStartSettingsActivity extends AppCompatActivity implements Adapt
         startSession.putExtra(TIA_GAIN_INDEX, mTiaGainIndex);
         startSession.putExtra(LOAD_RESISTOR_INDEX, mLoadResistorIndex);
         startSession.putExtra(INTERNAL_ZERO_INDEX, mInternalZeroIndex);
+
+        if (slope != 0 || intercept != 0) {
+            startSession.putExtra(SLOPE, slope);
+            startSession.putExtra(INTERCEPT, intercept);
+        }
+
         startActivity(startSession);
+    }
+
+    public void newCalibration(View view) {
+        Intent startNewCalibration = new Intent(this, CalibrationActivity.class);
+        startNewCalibration.putExtra(SELECTED_DEVICE, mSelectedDevice);
+        startNewCalibration.putExtra(BIAS_VOLTAGE_INDEX, mBiasVoltageIndex);
+        startNewCalibration.putExtra(BIAS_POLARITY_INDEX, mBiasPolarityIndex);
+        startNewCalibration.putExtra(TIA_GAIN_INDEX, mTiaGainIndex);
+        startNewCalibration.putExtra(LOAD_RESISTOR_INDEX, mLoadResistorIndex);
+        startNewCalibration.putExtra(INTERNAL_ZERO_INDEX, mInternalZeroIndex);
+        startActivityForResult(startNewCalibration, CALIBRATION_REQUEST_CODE);
+    }
+
+    // Request code for opening the calibration data
+    private static final int OPEN_FILE = 2;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void openFile(){
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("*/*");
+
+        startActivityForResult(intent, OPEN_FILE);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void loadCalibration(View view) {
+        openFile();
     }
 }
