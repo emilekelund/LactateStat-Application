3f421402d81c8bc88558e09324a3fada76c68749
Emil Ekelund <emil.ekelund@outlook.com>
2021-07-13 14:37:37 +0200

    New function when receiving combined data

--- app/src/main/java/com/example/lactatestat/utilities/BitConverter.java
+++ app/src/main/java/com/example/lactatestat/utilities/BitConverter.java
@@ -8,4 +8,12 @@ public class BitConverter {
         return (bytes[3] & 0xFF) << 24 | (bytes[2] & 0xFF) << 16 | (bytes[1] & 0xFF) << 8 | (bytes[0] & 0xFF);
     }
 
+    public static int[] bytesToIntArray(byte[] bytes) {
+        int[] combinedValues = new int[3];
+        combinedValues[0] = (bytes[7] & 0xFF) << 24 | (bytes[6] & 0xFF) << 16 | (bytes[5] << 8) | (bytes[4] & 0xFF);
+        combinedValues[1] = (bytes[3] & 0xFF) << 8 | (bytes[2] & 0xFF);
+        combinedValues[2] = (bytes[1] & 0xFF) << 8 | (bytes[0] & 0xFF);
+        return combinedValues;
+    }
+
 }
